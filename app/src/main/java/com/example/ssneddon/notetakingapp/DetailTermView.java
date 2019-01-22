707f647219c599c3b93d7417946b714e68d51a12
sethruss@gmail.com <ss128772>
2019-01-22 16:09:18 -0700

    new

--- app/src/main/java/com/example/ssneddon/notetakingapp/DetailTermView.java
+++ app/src/main/java/com/example/ssneddon/notetakingapp/DetailTermView.java
@@ -5,18 +5,24 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.example.ssneddon.notetakingapp.adapters.CustomCourseCursorAdapter;
 
 public class DetailTermView extends AppCompatActivity {
 
     private String termName, termStart, termEnd, termId, termFilter, action;
     private TextView textView_DetailTerm_End, textView_DetailTerm_Start;
     Button btn_DetailTerm_DeleteTerm;
+    ListView listView_TermDetails_NewCourse;
     Uri uri;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +32,7 @@ public class DetailTermView extends AppCompatActivity {
         btn_DetailTerm_DeleteTerm = findViewById(R.id.btn_DetailTerm_DeleteTerm);
         textView_DetailTerm_End = findViewById(R.id.textView_DetailTerm_End);
         textView_DetailTerm_Start = findViewById(R.id.textView_DetailTerm_Start);
-
+        listView_TermDetails_NewCourse = findViewById(R.id.listView_TermDetails_NewCourse);
 
         Intent intent = getIntent();
 
@@ -60,6 +66,9 @@ public class DetailTermView extends AppCompatActivity {
 
             setupDeleteButton();
 
+            buildCourseList();
+
+            setupNewCourseFAB();
         }
     }
 
@@ -80,6 +89,17 @@ public class DetailTermView extends AppCompatActivity {
                     }
                 }
         );
+    }
+
+    private void setupNewCourseFAB(){
+        FloatingActionButton fab_TermDetails_NewCourse = findViewById(R.id.fab_TermDetails_NewCourse);
+        fab_TermDetails_NewCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailTermView.this, NewCourse.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void createErrorDialog() {
@@ -106,5 +126,23 @@ public class DetailTermView extends AppCompatActivity {
         }
         c.close();
         return noAttachedCourses;
+    }
+
+    private void buildCourseList(){
+        cursor = getContentResolver().query(
+                DBProvider.CONTENT_URI_COURSES,
+                DataBaseContract.Course.PROJECTION,
+                "term_id=?",
+                new String[] {termId},
+                null
+        );
+
+        CustomCourseCursorAdapter customCourseCursorAdapter = new CustomCourseCursorAdapter(
+                DetailTermView.this,
+                cursor,
+                0
+        );
+
+        listView_TermDetails_NewCourse.setAdapter(customCourseCursorAdapter);
     }
 }
